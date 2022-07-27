c1a6766256598314a86fc74beb061c38f28269df
unknown <56641017+glorytiger@users.noreply.github.com>
2022-07-27 13:31:34 +0200

    now returns 50 random champion names

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ async function getSummonerInfo(summonerName) {
   .catch((err) => console.error(err));
 };
 
-async function getOwnedChampions(summonerId) {
+async function getOwnedChampionIds(summonerId) {
   return axios({
     method: 'get',
     url: `${process.env.HOST_PLATFORM}/lol/champion-mastery/v4/champion-masteries/by-summoner/${summonerId}`,
@@ -23,7 +23,7 @@ async function getOwnedChampions(summonerId) {
   .catch((err) => console.error(err));
 }
 
-async function getFreeChampions(summonerLevel) {
+async function getFreeChampionIds(summonerLevel) {
   return axios({
     method: 'get',
     url: process.env.HOST_PLATFORM + '/lol/platform/v3/champion-rotations',
@@ -33,16 +33,71 @@ async function getFreeChampions(summonerLevel) {
   .catch((err) => console.error(err));
 };
 
+async function getLatestVersion() {
+  return axios('https://ddragon.leagueoflegends.com/api/versions.json')
+  .then((res) => res.data[0])
+  .catch((err) => console.error(err));
+};
+
+async function getChampionNames(version) {
+  return axios(`https://ddragon.leagueoflegends.com/cdn/${version}/data/en_US/champion.json`)
+  .then((res) => res.data.data)
+  
+  // Filters key (id) and name
+  .then((map) => Object.entries(map).reduce((a,[k,v]) => {
+    a.push({ id: v.key, name: v.name });
+    return a;
+  }, []))
+  
+  // Sorts dictionary by id
+  .then((filteredObjects) => filteredObjects.sort((a,b) => a.id - b.id))
+  
+  // Returns an array of names where index is mapped to id
+  .then((sortedObjects) => {
+    let sortedArr = [];
+    let i = 0;
+    let id = 0;
+    let idMax = sortedObjects[sortedObjects.length - 1].id;
+    while (sortedArr.length <= idMax) {
+      if (parseInt(sortedObjects[i].id) === id) {
+        sortedArr.push(sortedObjects[i].name);
+        i++;
+      } else {
+        sortedArr.push('');
+      }
+      id++;
+    }
+
+    /*for (let i=0; i < sortedObjects.length; i++) console.log(i, ":", sortedObjects[i]);
+    console.log("sortedObjects.length:", sortedObjects.length);
+    for (let i=0; i < sortedArr.length; i++) console.log(i, ":", sortedArr[i]); 
+    console.log("sortedArr.length:", sortedArr.length);*/
+
+    return sortedArr;
+  })
+  .catch((err) => console.error(err));
+};
+
 async function run() {
   
   const summoner = await getSummonerInfo(process.env.SUMMONER_NAME);
 
-  const owned = await getOwnedChampions(summoner.id);
-  const free = await getFreeChampions(summoner.summonerLevel);
+  const owned = await getOwnedChampionIds(summoner.id);
+  const free = await getFreeChampionIds(summoner.summonerLevel);
   const playable = owned.concat(free);
   
-  console.log(playable);
+  const version = await getLatestVersion();
+  const names = await getChampionNames(version);
+
+  for (let i=0; i < 50; i++) {
+    const index = Math.floor(Math.random() * playable.length);
+    const id = playable[index];
+    console.log("id:", id, ", name:", names[id]);
+  }
 };
 
 run();
 
+// Get version and champion names
+// https://darkintaqt.com/blog/league-champ-id-list/
+ 
